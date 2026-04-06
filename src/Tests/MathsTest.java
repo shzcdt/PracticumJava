a138ab4afd01bd3bce751c9d57cbc15407dc8e2e
Ivan-Dubinov <dubinov.ivan@bk.ru>
2026-04-06 09:25:10 +0300

    feat: пример теста для проверки ошибки

--- src/Tests/MathsTest.java
+++ src/Tests/MathsTest.java
@@ -3,6 +3,9 @@ package Tests;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 public class MathsTest {
 
     @Test
@@ -14,5 +17,20 @@ public class MathsTest {
         int production = factor1 * factor2;
 
         Assertions.assertEquals(6, production, "production value is wrong");
+    }
+
+    @Test
+    public void shouldThrowException() {
+
+        final ArithmeticException exception = assertThrows(
+                ArithmeticException.class,
+                () -> badDivide()
+        );
+
+        assertEquals("/ by zero", exception.getMessage());
+    }
+
+    private int badDivide() {
+        return 10 / 0;
     }
 }
